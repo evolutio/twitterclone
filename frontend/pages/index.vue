b85c81c0da6514d87a5d42a884c1ffb03bf467c5
tonylampada <tonylampada@gmail.com>
2018-04-30 17:58:50 -0300

    roteamento simples (mapa do site)

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,7 +1,8 @@
 <template>
-  <v-layout justify-center align-center>
+  <!-- <v-layout justify-center align-center>
     <v-btn color="success" :to="{name: 'todos'}">What TODO next?</v-btn>
-  </v-layout>
+  </v-layout> -->
+  <h1>Isso aqui eh a timeline</h1>
 </template>
 
 <script>
---
+++ frontend/pages/user/_username/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>Isso aqui eh a timeline do usuario {{username}}</h1>
+</template>
+
+<script>
+
+export default {
+  asyncData (context) {
+    return {
+      username: context.params.username
+    }
+  },
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style>
+</style>
