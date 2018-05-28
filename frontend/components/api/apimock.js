03e6699ea7bcd171f48ec899b1bf1e59551298ad
tonylampada <tonylampada@gmail.com>
2018-05-28 15:41:34 -0300

    Carregando tweets assincronamente, sem SSR

--- frontend/components/api/apimock.js
+++ frontend/components/api/apimock.js
@@ -45,6 +45,34 @@ const api = {
                 {description: 'Walk the dog', done: false}
             ]
         });
+    },
+    list_tweets(){
+        return mockasync([
+            {
+              id: 1,
+              author_name: 'Isaac Newton',
+              author_username: '@isaacnewton',
+              author_avatar: 'http://1.bp.blogspot.com/-A9_ROvP0efw/TZI9dUsXAKI/AAAAAAAAGCI/rD_-a3ZBF3U/s1600/Isaac_Newton_Biography%255B1%255D.jpg',
+              created_at: '43 min',
+              text: 'A tendência dos corpos, quando nenhuma força é exercida sobre eles, é permanecer em seu estado natural, ou seja, repouso ou movimento retilíneo e uniforme.'
+            },
+            {
+              id: 2,
+              author_name: 'René Descartes',
+              author_username: '@descates',
+              author_avatar: 'http://www.filosofia.com.br/figuras/biblioteca/Descartes.jpg',
+              created_at: '1h 20min',
+              text: 'Penso, logo existo'
+            },
+            {
+              id: 3,
+              author_name: 'Albert Einstein',
+              author_username: '@einstein',
+              author_avatar: 'http://meioorc.com/wp-content/uploads/2015/07/como-vejo-o-mundo-einstein.jpg',
+              created_at: '2 dias',
+              text: 'Insanidade é continuar fazendo sempre a mesma coisa e esperar resultados diferentes'
+            }
+        ])
     }
 };
 
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,10 +1,14 @@
 <template>
-  <home :tweets="tweets"></home>
+  <div>
+    <v-progress-linear :indeterminate="true" v-if="!tweets"></v-progress-linear>
+    <home :tweets="tweets" v-if="tweets"></home>
+  </div>
 </template>
 
 <script>
 
 import home from '~/components/Home.vue'
+import AppApi from '~apijs'
 
 export default {
   components: {
@@ -12,33 +16,13 @@ export default {
   },
   data () {
     return {
-      tweets: [
-        {
-          id: 1,
-          author_name: 'Isaac Newton',
-          author_username: '@isaacnewton',
-          author_avatar: 'http://1.bp.blogspot.com/-A9_ROvP0efw/TZI9dUsXAKI/AAAAAAAAGCI/rD_-a3ZBF3U/s1600/Isaac_Newton_Biography%255B1%255D.jpg',
-          created_at: '43 min',
-          text: 'A tendência dos corpos, quando nenhuma força é exercida sobre eles, é permanecer em seu estado natural, ou seja, repouso ou movimento retilíneo e uniforme.'
-        },
-        {
-          id: 2,
-          author_name: 'René Descartes',
-          author_username: '@descates',
-          author_avatar: 'http://www.filosofia.com.br/figuras/biblioteca/Descartes.jpg',
-          created_at: '1h 20min',
-          text: 'Penso, logo existo'
-        },
-        {
-          id: 3,
-          author_name: 'Albert Einstein',
-          author_username: '@einstein',
-          author_avatar: 'http://meioorc.com/wp-content/uploads/2015/07/como-vejo-o-mundo-einstein.jpg',
-          created_at: '2 dias',
-          text: 'Insanidade é continuar fazendo sempre a mesma coisa e esperar resultados diferentes'
-        }
-      ]
+      tweets: null,
     }
+  },
+  mounted() {
+    AppApi.list_tweets().then(result => {
+      this.tweets = result.data
+    })
   }
 }
 </script>
